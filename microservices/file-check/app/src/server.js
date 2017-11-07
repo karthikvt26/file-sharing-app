34024e32d802882cfcf5bd85cb3d6a284d5ef22b
hasura buildbot <buildbot@hasura.io>
2017-11-07 16:44:22 +0530

    Adds a route to check for a valid req

--- microservices/file-check/app/src/server.js
+++ microservices/file-check/app/src/server.js
@@ -6,6 +6,10 @@ app.get('/', function (req, res) {
     res.send("Hello World!");
 });
 
+app.get('/check_req', function( req, res ) {
+  res.status(200).send('ok');
+});
+
 app.listen(8080, function () {
   console.log('Example app listening on port 8080!');
 });
