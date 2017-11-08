9cc6820f1e5012857669ad75302e5ad3efa261e6
hasura buildbot <buildbot@hasura.io>
2017-11-08 17:53:43 +0530

    WIP

--- microservices/file-check/app/src/server.js
+++ microservices/file-check/app/src/server.js
@@ -10,6 +10,28 @@ app.get('/check_req', function( req, res ) {
   console.log('Req body');
   console.log(req.body);
   console.log(req.query);
+
+  const file_id = req.query.file_id;
+
+  const user_id = req.headers['x-hasura-user-id'];
+
+  const queryObj = {
+    type: 'select',
+    args: {
+      table: 'user_file',
+      columns: ['id'],
+      where: {
+        'file_id': file_id,
+        'shared_users': {
+          'shared_user_id': parseInt(user_id, 10)
+        }
+      }
+    }
+  };
+
+  console.log('QueryObj');
+  console.log(queryObj);
+
   res.status(200).send('ok');
 });
 
